f7462c87dd3ff29ae96329a7c74b38c24d065413
triplee12 <tripleeoliver@gmail.com>
2023-08-08 14:48:34 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - aborts with status code 401
+    """
+    return abort(401, description='unauthorized')
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - aborts with status code 403
+    """
+    return abort(403, description='forbidden')
