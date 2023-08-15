ef973b53c8d220cd6ea650dc85d2adda3432ba6c
triplee12 <tripleeoliver@gmail.com>
2023-08-15 16:25:08 +0100

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """Hash password."""
 
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +11,24 @@ def _hash_password(password: str) -> bytes:
     encode_pwd = password.encode(encoding='UTF-8', errors='strict')
     hash_pwd = hashpw(password=encode_pwd, salt=gensalt())
     return hash_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        """Initialize the authentication."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """Register a user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hash_password = _hash_password(password=password)
+            new_user = self._db.add_user(
+                email=email, hashed_password=hash_password
+            )
+            return new_user
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,41 +1,57 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from db import DB
-from user import User
+"""Main file."""
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from auth import _hash_password
+# from db import DB
+# from user import User
+# from sqlalchemy.exc import InvalidRequestError
+# from sqlalchemy.orm.exc import NoResultFound
+# from auth import _hash_password
+from auth import Auth
 
-print(_hash_password("Hello Holberton"))
+email = 'me@me.com'
+password = 'mySecuredPwd'
 
-my_db = DB()
-
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
-
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
+auth = Auth()
 
 try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
 
 try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
 
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+# print(_hash_password("Hello Holberton"))
+
+# my_db = DB()
+
+# user = my_db.add_user("test@test.com", "PwdHashed")
+# print(user.id)
+
+# user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+# print(user_2.id)
+
+# find_user = my_db.find_user_by(email="test@test.com")
+# print(find_user.id)
+
+# try:
+#     find_user = my_db.find_user_by(email="test2@test.com")
+#     print(find_user.id)
+# except NoResultFound:
+#     print("Not found")
+
+# try:
+#     find_user = my_db.find_user_by(no_email="test@test.com")
+#     print(find_user.id)
+# except InvalidRequestError:
+#     print("Invalid")
+
+# try:
+#     my_db.update_user(user.id, hashed_password='NewPwd')
+#     print("Password updated")
+# except ValueError:
+#     print("Error")
