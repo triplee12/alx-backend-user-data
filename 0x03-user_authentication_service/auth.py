5c201d59968e26b05e1618742983ec89fc4c44d6
triplee12 <tripleeoliver@gmail.com>
2023-08-16 08:55:44 +0100

    Get reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,8 +83,8 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except NoResultFound:
-            raise ValueError
+        except NoResultFound as no_result:
+            raise ValueError from no_result
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Update password."""
